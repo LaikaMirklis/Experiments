2a9ea1a94789398f3b782bd76a79ec980bca644b
LaikaMirklis <olg.beam763@gmail.com>
2023-07-14 22:05:02 +0300

    01_exp v4 + problem solution

--- 01_Smooth color change/script.js
+++ 01_Smooth color change/script.js
@@ -1,4 +1,4 @@
-let inputText = prompt("Введіть текст");
+let inputText = prompt("Введіть текст", "З днем родини! (який був позавчора)");
 document.getElementById("input-text").innerHTML = inputText;
 
 let col;
@@ -6,16 +6,15 @@ let col;
 function changeColor() {
   const inputText = document.getElementById("input-text");
   const computedStyle = window.getComputedStyle(inputText);
-  const textColor = computedStyle.getPropertyValue("-webkit-text-fill-color");
+  const textColor = computedStyle.getPropertyValue("color");
 
   if (textColor === "rgb(255, 255, 255)") {
     col = "transparent";
     document.getElementById("button").innerHTML = "Не переливайся";
   } else {
-    col = "#FFFF";
+    col = "white";
     document.getElementById("button").innerHTML = "Переливайся";
   }
 
-  inputText.style.setProperty("-webkit-text-fill-color", col);
+  inputText.style.color = col;
 }
-
